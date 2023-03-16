658b3ac52fb18fbcd6c93d5f75aaca2e469ab82d
MrKakushonok <kukumber.laguta@mail.ru>
2023-03-16 16:17:32 +0500

    -remove redundant methods from Util class

--- src/main/java/gui/Util.java
+++ src/main/java/gui/Util.java
@@ -1,24 +1,12 @@
 package gui;
 
 public class Util {
-    static double distance(double x1, double y1, double x2, double y2)
-    {
-        double diffX = x1 - x2;
-        double diffY = y1 - y2;
-        return Math.sqrt(diffX * diffX + diffY * diffY);
-    }
-
     static double angleTo(double fromX, double fromY, double toX, double toY)
     {
         double diffX = toX - fromX;
         double diffY = toY - fromY;
 
         return asNormalizedRadians(Math.atan2(diffY, diffX));
-    }
-
-    static double applyLimits(double value, double min, double max)
-    {
-        return Math.max(min, Math.min(max, value));
     }
 
     static double asNormalizedRadians(double angle)
